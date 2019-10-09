dbf543203f8d45742afd76a7eb0920a6ecbad1c2
CleoniceSoares <35998428+CleoniceSoares@users.noreply.github.com>
2019-10-09 19:39:53 -0300

    Atualização

--- EscolaServiçoAPP/EscolaWebApp/JS/app.js
+++ EscolaServiçoAPP/EscolaWebApp/JS/app.js
@@ -21,3 +21,73 @@ let meuSegundoController = function($scope){
 }
 
 app.controller('MeuSegundoController', meuSegundoController);
+
+let alunoController = function() {
+    $scope.nome = "Nome";
+    $scope.matricula = "Matricula";
+    $scope.cpf = "CPF";
+    $scope.nascimento = ;
+    $scope.fk_id_endereco = 0;
+    $scope.fk_id_curso = 0;
+}
+
+app.controller("AlunoController", alunoController);
+
+let cursoController = function() {
+    $scope.nome = "Nome";
+    $scope.fk_id_turno = 0;
+}
+
+app.controller("CursoController", cursoController);
+
+let disciplinaController() {
+    $scope.nome = "Nome";
+    $scope.fk_id_professor = 0;
+}
+
+app.controller("DisciplinaController", disciplinaController);
+
+let turmaController = function() {
+    $scope.nome = "Nome";
+    $scope.fk_id_endereco = 0;
+}
+
+app.controller("TurmaController", turmaController);
+
+let enderecoController = function() {
+    $scope.logradouro = "Logradouro";
+    $scope.complemento = "Complemento";
+    $scope.bairro = "Bairro";
+    $scope.cep = "CEP";
+    $scope.numero = 0;
+}
+
+app.controller("EnderecoController", enderecoController);
+
+let escolaController = function() {
+    $scope.nome = "Nome";
+    $scope.fk_id_endereco = 0;
+    $scope.fk_id_campus = 0;
+}
+
+app.controller("EscolaController", escolaController);
+
+let campusController = function() {
+    $scope.sigla = "Sigla";
+    $scope.cidade = "Cidade";
+}
+
+app.controller("CampusController", campusController);
+
+let professorController = function() {
+    $scope.nome = "Nome";
+    $scope.fk_id_endereco = 0;
+}
+
+app.controller("ProfessorController", professorController);
+
+let turnoController = function() {
+    $scope.nome = "Nome";
+}
+
+app.controller("TurnoController", turnoController);

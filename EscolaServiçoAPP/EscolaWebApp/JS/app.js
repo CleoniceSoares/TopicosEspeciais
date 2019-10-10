57b2bedb83d0caa8877e18b49b51a75e10886efd
CleoniceSoares <35998428+CleoniceSoares@users.noreply.github.com>
2019-10-10 11:41:14 -0300

    Atualização AngularJS - Controlles

--- EscolaServiçoAPP/EscolaWebApp/JS/app.js
+++ EscolaServiçoAPP/EscolaWebApp/JS/app.js
@@ -22,72 +22,72 @@ let meuSegundoController = function($scope){
 
 app.controller('MeuSegundoController', meuSegundoController);
 
-let alunoController = function() {
-    $scope.nome = "Nome";
-    $scope.matricula = "Matricula";
-    $scope.cpf = "CPF";
-    $scope.nascimento = ;
+let alunoController = function($scope) {
+    $scope.nome = "";
+    $scope.matricula = "";
+    $scope.cpf = "";
+    $scope.nascimento = "";
     $scope.fk_id_endereco = 0;
     $scope.fk_id_curso = 0;
 }
 
 app.controller("AlunoController", alunoController);
 
-let cursoController = function() {
-    $scope.nome = "Nome";
+let cursoController = function($scope) {
+    $scope.nome = "";
     $scope.fk_id_turno = 0;
 }
 
 app.controller("CursoController", cursoController);
 
-let disciplinaController() {
-    $scope.nome = "Nome";
+let disciplinaController = function($scope) {
+    $scope.nome = "";
     $scope.fk_id_professor = 0;
 }
 
 app.controller("DisciplinaController", disciplinaController);
 
-let turmaController = function() {
-    $scope.nome = "Nome";
+let turmaController = function($scope) {
+    $scope.nome = "";
     $scope.fk_id_endereco = 0;
 }
 
 app.controller("TurmaController", turmaController);
 
-let enderecoController = function() {
-    $scope.logradouro = "Logradouro";
-    $scope.complemento = "Complemento";
-    $scope.bairro = "Bairro";
-    $scope.cep = "CEP";
+let enderecoController = function($scope) {
+    $scope.logradouro = "";
+    $scope.complemento = "";
+    $scope.bairro = "";
+    $scope.cep = "";
     $scope.numero = 0;
 }
 
 app.controller("EnderecoController", enderecoController);
 
-let escolaController = function() {
-    $scope.nome = "Nome";
+let escolaController = function($scope) {
+    $scope.nome = "";
     $scope.fk_id_endereco = 0;
     $scope.fk_id_campus = 0;
 }
 
 app.controller("EscolaController", escolaController);
 
-let campusController = function() {
-    $scope.sigla = "Sigla";
-    $scope.cidade = "Cidade";
+let campusController = function($scope) {
+    $scope.sigla = "";
+    $scope.cidade = "";
 }
 
 app.controller("CampusController", campusController);
 
-let professorController = function() {
-    $scope.nome = "Nome";
+let professorController = function($scope) {
+    $scope.nome = "";
     $scope.fk_id_endereco = 0;
 }
 
 app.controller("ProfessorController", professorController);
 
-let turnoController = function() {
-    $scope.nome = "Nome";
+let turnoController = function($scope) {
+    $scope.nome = "";
 }
 
 app.controller("TurnoController", turnoController);
